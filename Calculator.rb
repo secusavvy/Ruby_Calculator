b16d35c95fbd25e7022f6b10a1357c720729fd09
Zack <168565323+secusavvy@users.noreply.github.com>
2024-08-22 16:22:18 +0100

    Add files via upload

---
+++ Calculator.rb
@@ -0,0 +1,25 @@
+print "Enter the first number: "
+num1 = gets.chomp.to_f
+
+print "Enter the second number: "
+num2 = gets.chomp.to_f
+
+print "Enter the operator: "
+op = gets.chomp
+
+case op
+when "+"
+  puts "The answer is: #{num1 + num2}"
+when "-"
+  puts "The answer is: #{num1 - num2}"
+when "*"
+  puts "The answer is: #{num1 * num2}"
+when "/"
+  if num2 != 0
+    puts "The answer is: #{num1 / num2}"
+  else
+    puts "Error: Division by zero is not allowed."
+  end
+else
+  puts "Error: Invalid operator."
+end
